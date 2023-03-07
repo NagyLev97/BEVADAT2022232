00f0ecdbafd67b031f85058bc624fcb4a3c3ff34
Levente <nagylev97@gmail.com>
2023-03-07 11:23:11 +0100

    few tasks done

--- GYAK/GYAK02/GYAK02.py
+++ GYAK/GYAK02/GYAK02.py
@@ -16,8 +16,13 @@ import numpy as np
 #Ki: [[0,0],[0,0]]
 #create_array()
 
+def create_array(input_tuple=(2,2)):
+    arr = np.zeros(input_tuple)
+    return arr
 
 
+# arr = create_array()
+# print(arr)
 
 
 #Készíts egy függvényt ami a paraméterként kapott array-t főátlóját feltölti egyesekkel
@@ -25,12 +30,26 @@ import numpy as np
 #Ki: [[1,2],[3,1]]
 #set_one()
 
+def set_one(array: np.array):
+    np.fill_diagonal(array, 1)
+    return array
+
+# arr = [[1,2],[3,4]]
+# alma = set_one(np.array(arr))
+# print(alma)
 
 # Készíts egy függvényt ami transzponálja a paraméterül kapott mártix-ot:
 # Be: [[1, 2], [3, 4]]
 # Ki: [[1, 2], [3, 4]]
 # do_transpose()
 
+def do_transpose(array: np.array):
+    array = np.transpose(array)
+    return array
+
+# arr = [[1, 2], [3, 4]]
+# alma = do_transpose(np.array(arr))
+# print(alma)
 
 
 # Készíts egy olyan függvényt ami az array-ben lévő értékeket N tizenedjegyik kerekíti, ha nincs megadva ez a paraméter, akkor legyen az alapértelmezett a kettő 
@@ -38,17 +57,25 @@ import numpy as np
 # Ki: [0.12, 0.17]
 # round_array()
 
+def round_array(array: np.array, roundNumber=2):
+    array = np.round(array, roundNumber)
+    return array
 
-
+# array = round_array([0.1223, 0.1675], 2)
+# print(array)
 
 # Készíts egy olyan függvényt, ami a bementként kapott 0 és 1 ből álló tömben a 0 - False-ra, az 1 True-ra cserélni
 # Be: [[1, 0, 0], [1, 1, 1],[0, 0, 0]]
 # Ki: [[ True False False], [ True  True  True], [False False False]]
 # bool_array()
 
+def bool_array(array: np.array):
+    return array.astype(bool)
+    
 
+# array = bool_array(np.array([[1, 0, 0], [1, 1, 1],[0, 0, 0]]))
+# print(array)
 
-#
 # Készíts egy olyan függvényt, ami a bementként kapott 0 és 1 ből álló tömben a 1 - False-ra az 0 True-ra cserélni
 # Be: [[1, 0, 0], [1, 1, 1],[0, 0, 0]]
 # Ki: [[ True False False], [ True  True  True], [False False False]]
@@ -62,9 +89,4 @@ import numpy as np
 # Be: [[1,2], [3,4]]
 # Ki: [1,2,3,4]
 # flatten()
-
-
-# %%
-
-
 
