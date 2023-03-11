43c38ed1190e15ec3baf33884824ce99c5b87ae5
Levente <nagylev97@gmail.com>
2023-03-11 17:53:39 +0100

    tasks solved

--- HAZI/HAZI02/HAZI02.py
+++ HAZI/HAZI02/HAZI02.py
@@ -86,8 +86,54 @@ def eval_classification(input_list, input_array: np.array) -> str:
 # Ki: [-1,2,-1,4,-1,6]
 # replace_odd_numbers()
 
-def replace_odd_numbers(input_array: np.array):
+def replace_odd_numbers(input_array: np.array) -> np.array:
     return np.where(input_array % 2 == 1, -1, input_array)
     
 #print(replace_odd_numbers(np.array([1,2,3,4,5,6])))
 
+# Készíts egy olyan függvényt, ami egy array értékeit -1 és 1-re változtatja, attól függően, hogy az adott elem nagyobb vagy
+# kisebb a paraméterként megadott számnál.
+# Ha a szám kisebb mint a megadott érték, akkor -1, ha nagyobb vagy egyenlő, akkor pedig 1.
+# Be: [1, 2, 5, 0], 2
+# Ki: [-1, 1, 1, -1]
+# replace_by_value()
+
+def replace_by_value(input_array: np.array, num) -> np.array:
+    return np.where(input_array < num, -1, 1)
+
+#print(replace_by_value(np.array([1, 2, 5, 0]), 2))
+
+# Készíts egy olyan függvényt, ami egy array értékeit összeszorozza és az eredményt visszaadja
+# Be: [1,2,3,4]
+# Ki: 24
+# array_multi()
+# Ha több dimenziós a tömb, akkor az egész tömb elemeinek szorzatával térjen vissza
+
+def array_multi(input_array: np.array) -> int:
+    return np.prod(input_array)
+
+#print(array_multi(np.array([1,2,3,4])))
+
+# Készíts egy olyan függvényt, ami egy 2D array értékeit összeszorozza és egy olyan array-el tér vissza, aminek az elemei a soroknak a szorzata
+# Be: [[1, 2], [3, 4]]
+# Ki: [2, 12]
+# array_multi_2d()
+
+def array_multi_2d(input_array: np.array) -> np.array:
+    return np.prod(input_array, axis=1)
+
+#print(array_multi_2d(np.array([[1, 2], [3, 4]])))
+
+# Készíts egy olyan függvényt, amit egy meglévő numpy array-hez készít egy bordert nullásokkal. 
+# Bementként egy array-t várjon és kimenetként egy array jelenjen meg aminek van border-je
+# Be: [[1,2],[3,4]]
+# Ki: [[0,0,0,0],[0,1,2,0],[0,3,4,0],[0,0,0,0]]
+# add_border()
+
+def add_border(input_array: np.array) -> np.array:
+    shape = np.array(input_array.shape) + 2
+    output_array = np.zeros(shape)
+    output_array[1:-1, 1:-1] = input_array
+    return output_array
+
+print(add_border(np.array([[1,2],[3,4]])))
