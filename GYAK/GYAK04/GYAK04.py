f95628b3d6c1a1b19ee25a64590a0c0e3748469d
Levente <nagylev97@gmail.com>
2023-03-21 11:25:56 +0100

    .py add

--- GYAK/GYAK04/GYAK04.py
+++ GYAK/GYAK04/GYAK04.py
@@ -1,3 +1,4 @@
+# %%
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -6,6 +7,7 @@ import matplotlib.pyplot as plt
 FONTOS: Az első feladatáltal visszaadott DataFrame-et kell használni a további feladatokhoz. 
 A függvényeken belül mindig készíts egy másolatot a bemenő df-ről, (new_df = df.copy() és ezzel dolgozz tovább.)
 '''
+
 
 '''
 Készíts egy függvényt ami a bemeneti dictionary-ből egy DataFrame-et ad vissza.
@@ -17,10 +19,19 @@ függvény neve: dict_to_dataframe
 '''
 
 
+
+
 stats = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
        "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
        "area": [8.516, 17.10, 3.286, 9.597, 1.221],
        "population": [200.4, 143.5, 1252, 1357, 52.98] }
+
+def dict_to_dataframe(test_dict) -> pd.core.frame.DataFrame:
+    test_df = pd.DataFrame(test_dict)
+    return test_df
+
+# stat_dict = dict_to_dataframe(stats)
+# print(stat_dict)
 
 
 '''
@@ -34,6 +45,14 @@ függvény neve: get_column
 
 
 
+def get_column(test_df: pd.core.frame.DataFrame, area) -> pd.core.series.Series:
+    new_df = test_df.copy()
+    column = new_df[area]
+    return column
+
+#get_column(stat_dict, 'country')
+
+
 '''
 Készíts egy függvényt ami a bemeneti DataFrame-ből vissza adja a két legnagyobb területű országhoz tartozó sorokat.
 
@@ -44,7 +63,11 @@ függvény neve: get_top_two
 '''
 
 
+def get_top_two(test_df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+    new_df = test_df.copy()
+    return new_df.sort_values(by='area', ascending=False)[:2]
 
+#print(get_top_two(stat_dict))
 
 
 '''
@@ -58,7 +81,12 @@ függvény neve: population_density
 '''
 
 
+def population_density(test_df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+    new_df = test_df.copy()
+    new_df['density'] = new_df['population'] / new_df['area']
+    return new_df
 
+#population_density(stat_dict)
 
 
 '''
@@ -76,6 +104,20 @@ függvény neve: plot_population
 '''
 
 
+def plot_population(test_df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
+    new_df = test_df.copy()
+
+    fig, ax = plt.subplots()
+
+    ax.bar(new_df['country'], new_df['population'])
+
+    ax.set_xlabel('Population (millions)')
+    ax.set_ylabel('Country')
+    ax.set_title('Population of Countries')
+
+    return fig
+    
+#fig = plot_population(stat_dict)
 
 
 
@@ -90,6 +132,22 @@ Egy példa a kimenetre: fig
 return type: matplotlib.figure.Figure
 függvény neve: plot_area
 '''
+
+
+def plot_area(test_dict: pd.core.frame.DataFrame):
+    new_df = test_dict.copy()
+
+    fig, ax = plt.subplots()
+
+    x_value = new_df['country']
+    y_value = new_df['area']
+    ax.set_title('Area of Countries')
+
+    ax.pie(y_value, labels=x_value)
+
+    return fig
+
+#fig = plot_area(stat_dict)
 
 
 
