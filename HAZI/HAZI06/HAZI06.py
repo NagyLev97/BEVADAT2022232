bdd05b71d004b714bffe75bd6f8130b0bc8af5be
NagyLev97 <91882359+NagyLev97@users.noreply.github.com>
2023-04-16 04:14:32 +0200

    text writed

--- HAZI/HAZI06/HAZI06.py
+++ HAZI/HAZI06/HAZI06.py
@@ -12,36 +12,54 @@ Y = data.iloc[:, -1].values.reshape(-1,1)
 
 X_train, X_test, Y_train, Y_test  = train_test_split(X, Y, test_size=.2, random_state=41)
 
-# classifier = DecisionTreeClassifier(min_samples_split=3, max_depth=3)
-# classifier.fit(X_train, Y_train)
+classifier = DecisionTreeClassifier(min_samples_split=100, max_depth=11)
+classifier.fit(X_train, Y_train)
 
-# Y_pred = classifier.predict(X_test)
-# print(accuracy_score(Y_test, Y_pred))
+Y_pred = classifier.predict(X_test)
+print(accuracy_score(Y_test, Y_pred))
 
-S = [5, 10, 50, 100, 150]
-D = [8, 9, 10, 11, 12]
+# S = [5, 10, 50, 100, 150]
+# D = [8, 9, 10, 11, 12]
 
 # S = [2, 8, 15, 30, 45, 60, 100]
 # D = [3, 8, 10, 12, 15, 20, 30]
 
-results = []
-i = 0
-for choice in D:
-    j = 0
-    for choise_two in S:
-        try:
-            classifier = DecisionTreeClassifier(min_samples_split=S[j], max_depth=D[i])
-            classifier.fit(X_train, Y_train)
-            Y_pred = classifier.predict(X_test)
-            accuracy = accuracy_score(Y_test, Y_pred)
-            results.append(accuracy)
-            print(f'Min split: {S[j]}, Max depth: {D[i]}. The accuracy: {accuracy}')
-            j=j+1
-        except Exception:
-            print(f'ERROR: Min split: {S[j]}, Max depth: {D[i]}.')
-            j=j+1
-    i=i+1
+# results = []
+# i = 0
+# for choice in D:
+#     j = 0
+#     for choise_two in S:
+#         try:
+#             classifier = DecisionTreeClassifier(min_samples_split=S[j], max_depth=D[i])
+#             classifier.fit(X_train, Y_train)
+#             Y_pred = classifier.predict(X_test)
+#             accuracy = accuracy_score(Y_test, Y_pred)
+#             results.append(accuracy)
+#             print(f'Min split: {S[j]}, Max depth: {D[i]}. The accuracy: {accuracy}')
+#             j=j+1
+#         except Exception:
+#             print(f'ERROR: Min split: {S[j]}, Max depth: {D[i]}.')
+#             j=j+1
+#     i=i+1
     
+# A min_samples_split paraméter befolyásolja a fa modell komplexitását, illetve overfitting is jelentkezhet, ha például 
+# túl alacsony az érték, hiszen ekkor a fa sok ágat hoz létre, mellyel túl specifikus lesz a modell. 
+# Ugyanez vonatkozik a max_depth-re is, csak fordítva. Ha túl magas az érték, akkor juthatunk el gyorsan az overfittingig.
+# Ha viszont túl alacsony, akkor underfitting jelentkezhet. 
+# Ezekből kiindulva olyan stratégiát követtem az optimális fitting megválasztásához, hogy a min_split értékeket
+# nagyobb lépésközökkel, míg a max_depth-et kisebb lépésközökkel futtattam grid search segítségével. Ha a max_depth értéke 
+# kicsi (2,3,4), akkor a teszteseteken keresztül is látszik, hogy az accuracy ugyanakkora. Vagyis a jó stratégia, ha a 
+# max_depth értékét növelem. 
+# Ilyen módon jutottam el a 80,36%-os accuracy-ig, amit 100-as min_split-tel és 11-es max_depth-tel értem el.
+# Kíváncsi voltam arra is, hogy kisebb min_splittel is el lehet-e jutni hasonlóan jó eredményhez. 80,15% accuracy-t
+# lehetett elérni 15 min_split és 20 max_depth megválasztásával. 
+# A tesztesetek futtatásánál kiderült, hogy több esetben hibára fut az algoritmus, ezért egy try-catch blokkot tettem
+# a kódba. 
+
+# ------------------------------------------------------------------
+
+#Tesztesetek:
+
 # Min split: 5, Max depth: 2. The accuracy: 0.7823333333333333
 # Min split: 10, Max depth: 2. The accuracy: 0.7823333333333333
 # Min split: 50, Max depth: 2. The accuracy: 0.7823333333333333
@@ -78,6 +96,7 @@ for choice in D:
 # Min split: 100, Max depth: 7. The accuracy: 0.7940833333333334
 # Min split: 150, Max depth: 7. The accuracy: 0.793
 
+# ERROR: Min split: 5, Max depth: 8.
 # Min split: 10, Max depth: 8. The accuracy: 0.7955833333333333
 # Min split: 50, Max depth: 8. The accuracy: 0.7954166666666667
 # Min split: 100, Max depth: 8. The accuracy: 0.7969166666666667
@@ -98,7 +117,7 @@ for choice in D:
 # ERROR: Min split: 5, Max depth: 11.
 # ERROR: Min split: 10, Max depth: 11.
 # Min split: 50, Max depth: 11. The accuracy: 0.8026666666666666
-# Min split: 100, Max depth: 11. The accuracy: 0.8035833333333333
+# Min split: 100, Max depth: 11. The accuracy: 0.8035833333333333 <--
 # Min split: 150, Max depth: 11. The accuracy: 0.80075
 
 # ERROR: Min split: 5, Max depth: 12.
@@ -150,7 +169,7 @@ for choice in D:
 
 # Min split: 2, Max depth: 20. The accuracy: 0.7839166666666667
 # Min split: 8, Max depth: 20. The accuracy: 0.7955
-# Min split: 15, Max depth: 20. The accuracy: 0.8015
+# Min split: 15, Max depth: 20. The accuracy: 0.8015                <--
 # Min split: 30, Max depth: 20. The accuracy: 0.8016666666666666
 # Min split: 45, Max depth: 20. The accuracy: 0.7949166666666667
 # Min split: 60, Max depth: 20. The accuracy: 0.7896666666666666
