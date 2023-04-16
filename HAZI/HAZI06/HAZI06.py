b93c04d0089f4cf333f3e540bb94e5b9b8d96c4d
NagyLev97 <91882359+NagyLev97@users.noreply.github.com>
2023-04-16 03:45:19 +0200

    tests added

--- HAZI/HAZI06/HAZI06.py
+++ HAZI/HAZI06/HAZI06.py
@@ -18,21 +18,148 @@ X_train, X_test, Y_train, Y_test  = train_test_split(X, Y, test_size=.2, random_
 # Y_pred = classifier.predict(X_test)
 # print(accuracy_score(Y_test, Y_pred))
 
-D = [5, 10, 50, 100, 150]
-S = [2, 3, 4, 5, 6, 7, 8, 9]
+S = [5, 10, 50, 100, 150]
+D = [8, 9, 10, 11, 12]
+
+# S = [2, 8, 15, 30, 45, 60, 100]
+# D = [3, 8, 10, 12, 15, 20, 30]
 
 results = []
 i = 0
 for choice in D:
     j = 0
     for choise_two in S:
-        classifier = DecisionTreeClassifier(min_samples_split=S[j], max_depth=D[i])
-        classifier.fit(X_train, Y_train)
-        Y_pred = classifier.predict(X_test)
-        accuracy = accuracy_score(Y_test, Y_pred)
-        results.append(accuracy)
-        print(f'Min split: {S[j]}, Max depth: {D[i]}. The accuracy: {accuracy}')
-        j=j+1
+        try:
+            classifier = DecisionTreeClassifier(min_samples_split=S[j], max_depth=D[i])
+            classifier.fit(X_train, Y_train)
+            Y_pred = classifier.predict(X_test)
+            accuracy = accuracy_score(Y_test, Y_pred)
+            results.append(accuracy)
+            print(f'Min split: {S[j]}, Max depth: {D[i]}. The accuracy: {accuracy}')
+            j=j+1
+        except Exception:
+            print(f'ERROR: Min split: {S[j]}, Max depth: {D[i]}.')
+            j=j+1
     i=i+1
     
+# Min split: 5, Max depth: 2. The accuracy: 0.7823333333333333
+# Min split: 10, Max depth: 2. The accuracy: 0.7823333333333333
+# Min split: 50, Max depth: 2. The accuracy: 0.7823333333333333
+# Min split: 100, Max depth: 2. The accuracy: 0.7823333333333333
+# Min split: 150, Max depth: 2. The accuracy: 0.7823333333333333
 
+# Min split: 5, Max depth: 3. The accuracy: 0.7839166666666667
+# Min split: 10, Max depth: 3. The accuracy: 0.7839166666666667
+# Min split: 50, Max depth: 3. The accuracy: 0.7839166666666667
+# Min split: 100, Max depth: 3. The accuracy: 0.7839166666666667
+# Min split: 150, Max depth: 3. The accuracy: 0.7839166666666667
+
+# Min split: 5, Max depth: 4. The accuracy: 0.7849166666666667
+# Min split: 10, Max depth: 4. The accuracy: 0.7849166666666667
+# Min split: 50, Max depth: 4. The accuracy: 0.7849166666666667
+# Min split: 100, Max depth: 4. The accuracy: 0.7849166666666667
+# Min split: 150, Max depth: 4. The accuracy: 0.7846666666666666
+
+# Min split: 5, Max depth: 5. The accuracy: 0.7885833333333333
+# Min split: 10, Max depth: 5. The accuracy: 0.7885833333333333
+# Min split: 50, Max depth: 5. The accuracy: 0.7885833333333333
+# Min split: 100, Max depth: 5. The accuracy: 0.7885833333333333
+# Min split: 150, Max depth: 5. The accuracy: 0.78825
+
+# Min split: 5, Max depth: 6. The accuracy: 0.7885
+# Min split: 10, Max depth: 6. The accuracy: 0.7885
+# Min split: 50, Max depth: 6. The accuracy: 0.7885
+# Min split: 100, Max depth: 6. The accuracy: 0.7888333333333334
+# Min split: 150, Max depth: 6. The accuracy: 0.7884166666666667
+
+# Min split: 5, Max depth: 7. The accuracy: 0.7934166666666667
+# Min split: 10, Max depth: 7. The accuracy: 0.7935
+# Min split: 50, Max depth: 7. The accuracy: 0.7935833333333333
+# Min split: 100, Max depth: 7. The accuracy: 0.7940833333333334
+# Min split: 150, Max depth: 7. The accuracy: 0.793
+
+# Min split: 10, Max depth: 8. The accuracy: 0.7955833333333333
+# Min split: 50, Max depth: 8. The accuracy: 0.7954166666666667
+# Min split: 100, Max depth: 8. The accuracy: 0.7969166666666667
+# Min split: 150, Max depth: 8. The accuracy: 0.7955833333333333
+
+# ERROR: Min split: 5, Max depth: 9.
+# ERROR: Min split: 10, Max depth: 9.
+# Min split: 50, Max depth: 9. The accuracy: 0.7981666666666667
+# Min split: 100, Max depth: 9. The accuracy: 0.7986666666666666
+# Min split: 150, Max depth: 9. The accuracy: 0.7973333333333333
+
+# ERROR: Min split: 5, Max depth: 10.
+# ERROR: Min split: 10, Max depth: 10.
+# Min split: 50, Max depth: 10. The accuracy: 0.8010833333333334
+# Min split: 100, Max depth: 10. The accuracy: 0.80225
+# Min split: 150, Max depth: 10. The accuracy: 0.7991666666666667
+
+# ERROR: Min split: 5, Max depth: 11.
+# ERROR: Min split: 10, Max depth: 11.
+# Min split: 50, Max depth: 11. The accuracy: 0.8026666666666666
+# Min split: 100, Max depth: 11. The accuracy: 0.8035833333333333
+# Min split: 150, Max depth: 11. The accuracy: 0.80075
+
+# ERROR: Min split: 5, Max depth: 12.
+# ERROR: Min split: 10, Max depth: 12.
+# Min split: 50, Max depth: 12. The accuracy: 0.8
+# Min split: 100, Max depth: 12. The accuracy: 0.8025
+# Min split: 150, Max depth: 12. The accuracy: 0.80025
+
+# ------------------------------------------------------------------
+
+# ERROR: Min split: 8, Max depth: 3.
+# ERROR: Min split: 15, Max depth: 3.
+# ERROR: Min split: 30, Max depth: 3.
+# ERROR: Min split: 45, Max depth: 3.
+# ERROR: Min split: 60, Max depth: 3.
+# ERROR: Min split: 100, Max depth: 3.
+
+# Min split: 2, Max depth: 8. The accuracy: 0.7839166666666667
+# Min split: 8, Max depth: 8. The accuracy: 0.7955
+# ERROR: Min split: 15, Max depth: 8.
+# ERROR: Min split: 30, Max depth: 8.
+# ERROR: Min split: 45, Max depth: 8.
+# ERROR: Min split: 60, Max depth: 8.
+# ERROR: Min split: 100, Max depth: 8.
+
+# Min split: 2, Max depth: 10. The accuracy: 0.7839166666666667
+# Min split: 8, Max depth: 10. The accuracy: 0.7956666666666666
+# ERROR: Min split: 15, Max depth: 10.
+# ERROR: Min split: 30, Max depth: 10.
+# ERROR: Min split: 45, Max depth: 10.
+# ERROR: Min split: 60, Max depth: 10.
+# ERROR: Min split: 100, Max depth: 10.
+
+# Min split: 2, Max depth: 12. The accuracy: 0.7839166666666667
+# Min split: 8, Max depth: 12. The accuracy: 0.7955
+# Min split: 15, Max depth: 12. The accuracy: 0.80075
+# Min split: 30, Max depth: 12. The accuracy: 0.7995
+# Min split: 45, Max depth: 12. The accuracy: 0.7929166666666667
+# Min split: 60, Max depth: 12. The accuracy: 0.78425
+# Min split: 100, Max depth: 12. The accuracy: 0.7818333333333334
+
+# Min split: 2, Max depth: 15. The accuracy: 0.7839166666666667
+# Min split: 8, Max depth: 15. The accuracy: 0.7953333333333333
+# Min split: 15, Max depth: 15. The accuracy: 0.8015
+# Min split: 30, Max depth: 15. The accuracy: 0.79975
+# Min split: 45, Max depth: 15. The accuracy: 0.7931666666666667
+# Min split: 60, Max depth: 15. The accuracy: 0.7865833333333333
+# Min split: 100, Max depth: 15. The accuracy: 0.7843333333333333
+
+# Min split: 2, Max depth: 20. The accuracy: 0.7839166666666667
+# Min split: 8, Max depth: 20. The accuracy: 0.7955
+# Min split: 15, Max depth: 20. The accuracy: 0.8015
+# Min split: 30, Max depth: 20. The accuracy: 0.8016666666666666
+# Min split: 45, Max depth: 20. The accuracy: 0.7949166666666667
+# Min split: 60, Max depth: 20. The accuracy: 0.7896666666666666
+# Min split: 100, Max depth: 20. The accuracy: 0.7880833333333334
+
+# Min split: 2, Max depth: 30. The accuracy: 0.7839166666666667
+# Min split: 8, Max depth: 30. The accuracy: 0.7969166666666667
+# Min split: 15, Max depth: 30. The accuracy: 0.80225
+# Min split: 30, Max depth: 30. The accuracy: 0.8025
+# Min split: 45, Max depth: 30. The accuracy: 0.7985
+# Min split: 60, Max depth: 30. The accuracy: 0.7974166666666667
+# Min split: 100, Max depth: 30. The accuracy: 0.7973333333333333
