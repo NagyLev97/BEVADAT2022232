b6e3b3d74790d1ca21d09a951245b5ab1c26e89b
NagyLev97 <91882359+NagyLev97@users.noreply.github.com>
2023-04-24 20:53:49 +0200

    gyak08 done

--- GYAK/GYAK08/GYAK08.py
+++ GYAK/GYAK08/GYAK08.py
@@ -1,31 +1,49 @@
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
+from LinearRegressionSkeleton import LinearRegression
+from matplotlib import pyplot as plt
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+X = df['petal width (cm)'].values
+y = df['sepal length (cm)'].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+lr = LinearRegression(epochs = 10000, lr = 0.001)
+lr.fit(X_train, y_train)
+y_pred = lr.predict(X_test)
+
+plt.scatter(X_test, y_test)
+plt.plot([min(X), max(X)], [min(y_pred), max(y_pred)], color='red') 
+plt.show()
 
 
-class LinearRegression:
-    def __init__(self, epochs: int = 1000, lr: float = 1e-3):
-        self.epochs = epochs
-        self.lr = lr
-        self.m = 0 #meredekség
-        self.c = 0 #metszés az x tengellyel
+# class LinearRegression:
+#     def __init__(self, epochs: int = 1000, lr: float = 1e-3):
+#         self.epochs = epochs
+#         self.lr = lr
+#         self.m = 0 #meredekség
+#         self.c = 0 #metszés az x tengellyel
 
-        def fit(self, X: np.array, y: np.array):
-            n = float(len(X))
+#         def fit(self, X: np.array, y: np.array):
+#             n = float(len(X))
 
-            losses = []
-            for i in range(self.epochs):
-                y_pred = self.m*X + self.c
+#             losses = []
+#             for i in range(self.epochs):
+#                 y_pred = self.m*X + self.c
 
-                residuals = y_pred - y
-                loss = np.sum(residuals ** 2)
-                losses.append(loss)
-                D_m = (-2/n) * sum(X * residuals)
-                D_c = (-2/n) * sum(residuals)
-                self.m = self.m + self.lr * D_m
-                self.c = self.c + self.lr * D_c
+#                 residuals = y_pred - y
+#                 loss = np.sum(residuals ** 2)
+#                 losses.append(loss)
+#                 D_m = (-2/n) * sum(X * residuals)
+#                 D_c = (-2/n) * sum(residuals)
+#                 self.m = self.m + self.lr * D_m
+#                 self.c = self.c + self.lr * D_c
 
-    def predict(self, X):
-        return self.m*X + self.c
+#     def predict(self, X):
+#         return self.m*X + self.c
 
     # def fit(self, X: np.array, y: np.array):
     #     self.X = X
